ab403905419fa6dd86db3e4bd9dbfe7ac6a8646d
Fernando Salamanca <67641798+FerMusicComposer@users.noreply.github.com>
2021-08-30 22:36:48 +0200

    Fixed stuck suit class issue Added number color change depending on suit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('button');
 const card = document.querySelector('.card');
-const cardNumber = document.querySelector('.card-number');
+const cardNumber = document.querySelector('#cardNumber');
+const suits = ['spades', 'clubs', 'diamonds', 'hearts'];
+const cardNumberBlack = 'card-number';
+const cardNumberRed = 'card-number-red';
 
 const randomNumberGenerator = () => {
     let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
@@ -9,11 +12,20 @@ const randomNumberGenerator = () => {
     return numbers[randomNumber];
 };
 
-const randomSuitGenerator = () => {
-    let suits = ['spades', 'clubs', 'diamonds', 'hearts'];
+const randomSuitGenerator = suits => {
     let randomSuit = Math.floor(Math.random() * suits.length);
     console.log(suits[randomSuit]);
+
     return suits[randomSuit];
+};
+
+const resetSuitAndNumberClasses = () => {
+    for (let index = 0; index < suits.length; index++) {
+        if (card.classList.contains(suits[index])) {
+            card.classList.remove(suits[index]);
+            cardNumber.classList.remove(cardNumberBlack, cardNumberRed);
+        }
+    }
 };
 
 window.onload = () => {
@@ -24,13 +36,26 @@ window.onload = () => {
 
 const getRandomCard = () => {
     button.addEventListener('click', () => {
+        //Cleans the previously selected suit and number classes
+        resetSuitAndNumberClasses();
         card.classList.remove('card-reverse');
-        card.classList.add('card-front', randomSuitGenerator());
+        card.classList.add('card-front', randomSuitGenerator(suits));
+
+        //Displays the number as red or black depending on the suit
+        if (card.classList.contains('hearts') || card.classList.contains('diamonds')) {
+            cardNumber.classList.add(cardNumberRed);
+        } else {
+            cardNumber.classList.add(cardNumberBlack);
+        }
+
         cardNumber.innerHTML = randomNumberGenerator();
+
+        //"Flips" the card back to reverse
         setTimeout(() => {
+            resetSuitAndNumberClasses();
             card.classList.remove('card-front');
             card.classList.add('card-reverse');
             cardNumber.innerHTML = '';
-        }, 3000);
+        }, 1000);
     });
 };
